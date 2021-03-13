c3b4a7b12c688af1471ee2edd96c9580718c5f83
Carlos Gómez <caragomezher@unal.edu.co>
2021-03-13 22:37:54 +0000

    Add pokemon event

--- src/components/pages/Contact.svelte
+++ src/components/pages/Contact.svelte
@@ -3,40 +3,90 @@
 
   let pokemon_list;
   let pokemon_results = [];
-  let pokemon_loaded = false;
+
+  let columnNumber = 10;
+  let offset = 0;
+
+  import Pokecard from  './Contact/Pokecard.svelte';
 
   onMount(async () => {
-    await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+    await loadPokemon()
+  });
+
+  async function loadPokemon(){
+    pokemon_results = [];
+    await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${columnNumber}&offset=${offset}`)
       .then((response) => response.json())
       .then((data) => {
         pokemon_list = data;
         recoverPokemon();
       });
-  });
+  }
 
   function recoverPokemon() {
-    pokemon_list.results.forEach((element) => {
+    Promise.all(pokemon_list.results.map((element) => 
       fetch(element.url)
         .then((response2) => response2.json())
         .then((data2) => {
-          pokemon_results.push(data2);
-          console.log(pokemon_results);
-          pokemon_loaded = true;
-        });
+            return data2;
+        })
+    )).then(data => {
+        pokemon_results = data;
     });
   }
+
+  async function nextPokemonGroup(){
+    offset += columnNumber;
+    await loadPokemon();
+  }
+
+  async function backPokemonGroup(){
+    offset -= columnNumber;
+    await loadPokemon();
+  }
+
 </script>
 
-<p>Contact page!</p>
 
-{#if pokemon_loaded}
+
+<h2>Pokemon page!</h2>
+
+<div class="min-bar">
+{#if offset > 0}
+  <button on:click={backPokemonGroup}>Back</button>
+{/if}
+<input id="columnNumber" type=number bind:value={columnNumber} min="10" step="10" max="100" />
+<button on:click={nextPokemonGroup}>Next</button>
+</div>
+
+
+<div class="pokemon-results">
   {#each pokemon_results as pokemon}
-    <p>{pokemon.name}</p>
-    <img
-      src={pokemon.sprites.other["official-artwork"].front_default}
-      alt="image_{pokemon.name}"
-    />
+    <Pokecard pokemon={pokemon}/>
   {:else}
     <p>Loading...</p>
   {/each}
-{/if}
+</div>
+
+
+
+<style>
+  .pokemon-results {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  }
+
+  .min-bar{
+    padding: 0.5rem 1rem;
+    margin-bottom: 1rem;
+    border-radius: 6px;
+    border: 1px solid var(--text);
+    display: flex;
+    
+  }
+
+  #columnNumber{
+    width: 3rem;
+  }
+</style>
